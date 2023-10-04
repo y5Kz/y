3641bae867b7adf7d15c9b26ce68b1932d982834
Yusuke Kino <k1853@icloud.com>
2023-10-04 20:42:46 +0900

    a

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
 module.exports = {
   outputDir: 'docs',
   publicPath: './'
